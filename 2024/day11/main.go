8c1314c7260cea547418fb126efb585e99d608af
DavidDoyle20 <davestar80@gmail.com>
2024-12-28 11:39:48 -0600

    day 11 part two

--- 2024/day11/main.go
+++ 2024/day11/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
+var cache = make(map[string]int)
+
 type Node struct {
-	data string
+	data int
 	next *Node
 }
 
@@ -17,11 +20,33 @@ type LinkedList struct {
 	head *Node
 }
 
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var stones []string
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	stones = strings.Split(scanner.Text(), " ")
+
+	var ll LinkedList
+	for _, s := range stones {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+		ll.Append(num)
+	}
+	fmt.Println("Part One: ", partOne(ll, 25))
+	fmt.Println("Part Two: ", partTwo(ll, 75))
+}
+
 func (ll *LinkedList) Length() int {
-	var count int
-	current := ll.head
-	for current != nil {
-		current = current.next
+	count := 0
+	for current := ll.head; current != nil; current = current.next {
 		count++
 	}
 	return count
@@ -54,16 +79,11 @@ func (ll *LinkedList) Copy() *LinkedList {
 		newCurrent.next = newNode
 		newCurrent = newNode
 	}
-	fmt.Println("Done Copying")
+	//fmt.Println("Done Copying")
 	return newList
 }
 
-func (ll *LinkedList) Prepend(data string) {
-	newNode := &Node{data: data, next: ll.head}
-	ll.head = newNode
-}
-
-func (ll *LinkedList) Append(data string) {
+func (ll *LinkedList) Append(data int) {
 	newNode := &Node{data: data, next: nil}
 
 	if ll.head == nil {
@@ -78,19 +98,7 @@ func (ll *LinkedList) Append(data string) {
 	current.next = newNode
 }
 
-func (ll *LinkedList) AppendNode(node *Node) {
-	if ll.head == nil {
-		ll.head = node
-		return
-	}
-
-	current := ll.head
-	for current.next != nil {
-		current = current.next
-	}
-	current.next = node
-}
-
+// for testing
 func (ll *LinkedList) PrintList() {
 	current := ll.head
 	for current != nil {
@@ -103,47 +111,24 @@ func (ll *LinkedList) PrintList() {
 // split one node into two
 // data of node = front, next = new node with back and its tail being the tail of the original
 func (node *Node) splitNode() {
-	if len(node.data)%2 != 0 {
+	digits := countDigit(node.data)
+	if digits%2 != 0 {
 		return
 	}
-	mid := len(node.data) / 2
-	left := node.data[:mid]
-	right := node.data[mid:]
-	left = strings.TrimLeft(left, "0")
-	right = strings.TrimLeft(right, "0")
+	halfDigits := digits / 2
+	divisor := int(math.Pow10(halfDigits))
 
-	if len(left) == 0 {
-		left = "0"
-	}
-	if len(right) == 0 {
-		right = "0"
-	}
+
+	left := node.data / divisor
+	right := node.data % divisor
 
 	node.data = left
 	nextNode := node.next
-
-	newNode := &Node{data: right, next: nextNode}
-	node.next = newNode
+	node.next = &Node{data: right, next: nextNode}
 }
 
-func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	var stones []string
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	stones = strings.Split(scanner.Text(), " ")
-
-	var ll LinkedList
-	for _, s := range stones {
-		ll.Append(s)
-	}
-	//fmt.Println("Part One: ", partOne(ll, 25))
-	fmt.Println("Part Two: ", partOne(ll, 75))
+func countDigit(n int) int {
+	return int(math.Floor(math.Log10((float64(n)))+1))
 }
 
 func partOne(stones LinkedList, blinks int) int {
@@ -152,21 +137,74 @@ func partOne(stones LinkedList, blinks int) int {
 	for i := 0; i < blinks; i++ {
 		current := llist.head
 		for current != nil {
-			if current.data == "0" {
-				current.data = "1"
-			} else if len(current.data)%2 == 0 {
+			if current.data == 0 {
+				current.data = 1
+			} else if countDigit(current.data)%2 == 0 {
 				current.splitNode()
 				current = current.next
 			} else {
-				num, err := strconv.Atoi(current.data)
-				if err != nil {
-					return 0
-				}
-				num *= 2024
-				current.data = strconv.Itoa(num)
+				current.data *= 2024
 			}
 			current = current.next
 		}
 	}
 	return llist.Length()
 }
+
+// calculate for each item in the list and free after that
+func partTwo(stones LinkedList, blinks int) int {
+	count := 0
+	for current := stones.head; current != nil; current = current.next {
+		//fmt.Println("Processing", int(current.data))
+		count += temp(current.data, blinks)
+	}
+	return count
+}
+
+// returns the size a list would be starting from the number n
+func temp(n int, blinks int) int {
+	//fmt.Println(n, blinks)
+	if blinks == 0 {
+		return 1
+	}
+	key := fmt.Sprintf("%d_%d", n, blinks)
+	if val, ok := cache[key]; ok {
+		return val
+	}
+
+	count := 0
+	if blinks != 0 {
+		blinks--
+		if n == 0 {
+			count += temp(1, blinks)
+		} else if countDigit(n)%2 == 0 {
+			left, right := splitInt(n)
+			count += temp(left, blinks) + temp(right, blinks)
+		} else {
+			count += temp(n*2024, blinks)
+		}
+	}
+
+	cache[key] = count
+	return count
+}
+
+func splitInt(n int) (int, int) {
+	digits := countDigit(n)
+	if digits%2 != 0 {
+		return 0, 0
+	}
+	halfDigits := digits / 2
+	divisor := int(math.Pow10(halfDigits))
+
+
+	left := n / divisor
+	right := n % divisor
+
+	return left, right
+}
+
+
+
+
+
